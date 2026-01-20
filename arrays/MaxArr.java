e152c0de8ff8ad45a9dcfef094ac9a736509a31d
Banu Teja Challa <banuteja2005@gmail.com>
2026-01-20 23:49:30 +0530

    Added comments for the MaxArr program

--- arrays/MaxArr.java
+++ arrays/MaxArr.java
@@ -10,14 +10,20 @@ class MaxArr
         {
             arr[i] = scn.nextInt();
         }
+	// Everything until here is the same as creating and taking inputs for an array.
+	// we declare a new variable max and assign the first element of the array as the maximum element.
         int max = arr[0];
         for (int i = 1; i < size; i++)
+	//loop starts from index 1, since index 0 is already assigned max.
         {
             if(max < arr[i])
             {
                 max = arr[i];
+		//if the ith index element of the array is greater than the current max element, it is replaced. else it remains the same
             }
         }
+	//do the same until all the elements in the array are traversed.
         System.out.println(max);
+	//print the max element
     }
 }
