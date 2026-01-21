181a39384c0251a190a58b80846e6edcd806bd48
Banu Teja Challa <banuteja2005@gmail.com>
2026-01-21 22:52:14 +0530

    Added comments to the RevArray program

--- arrays/RevArray.java
+++ arrays/RevArray.java
@@ -12,19 +12,21 @@ class RevArray
         {
             arr[i] = scn.nextInt();
         }      
+	//create two variables that act like pointers pointing to the either ends of the array initially.
+        int left = 0; // left is set to first element
+        int right = size - 1; // right is set to the last element
 
-        int left = 0;
-        int right = size - 1;
-
-        while (left < right)
+        while (left < right) // this loop runs until the index of left is smaller than the index of right
         {
-            int temp = arr[left];
+	    //basic swap mechanism
+            int temp = arr[left]; 
             arr[left] = arr[right]; 
             arr[right] = temp;
+	    //update left and right to refer the right indices
             left++;
             right--;
         }
-
+	// print the array
         for(int i = 0; i < size; i ++)
         {
             System.out.print(arr[i] + " ");
