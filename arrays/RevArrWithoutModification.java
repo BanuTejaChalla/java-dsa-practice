bc70d64caa40153ca99b75583d4546616331f00f
Banu Teja Challa <banuteja2005@gmail.com>
2026-01-21 22:58:55 +0530

    Added comments to the program to revese an array without modifying the actual array

--- arrays/RevArrWithoutModification.java
+++ arrays/RevArrWithoutModification.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class RevArrWithoutModification
 {
-	public static void main (String[] args)
-	{
-		Scanner scn = new Scanner(System.in);
-		int size = scn.nextInt();
-		int[] arr = new int[size];
-		
-        for (int i = 0; i < size; i++)
-        {
-            arr[i] = scn.nextInt();
-        }
-        
-        for (int i = size - 1; i >= 0; i--)
-        {
-            System.out.print(arr[i] + " ");
-        }
-	}
+    public static void main (String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+	int size = scn.nextInt();
+	int[] arr = new int[size];
+	
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = scn.nextInt();
+    }
+
+    //prints the array is a reverse order starting from the index size - 1 
+    for (int i = size - 1; i >= 0; i--)
+    {
+        System.out.print(arr[i] + " ");
+    }
+    }
 }
