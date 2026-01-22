1c8c545cf63f3bcae4c8b0903494ef14f7431ad1
Banu Teja Challa <banuteja2005@gmail.com>
2026-01-22 23:34:53 +0530

    Added comments to the program that checks if a given array is Strictly Sorted

--- arrays/IsStrictlySorted.java
+++ arrays/IsStrictlySorted.java
@@ -6,6 +6,7 @@ class IsStrictlySorted
 	Scanner scn = new Scanner(System.in);
 	int size = scn.nextInt();
 	int[] arr = new int[size];
+	//assumes that the array is already sorted.
 	boolean isTrue = true;
 	
 	for (int i = 0; i < size; i++)
@@ -15,8 +16,10 @@ class IsStrictlySorted
 	
 	for(int i = 0; i < size-1; i++)
 	{
+	    //Everything same as the normal sort check, but the only difference in Strictly Sorted is, even equal values count as unsorted values
 	    if (arr[i] >= arr[i+1])
 	    {
+		//if any pair found, then mark the boolean variable as false and print that in the final output
 	        isTrue = false;
 	    }
 	}
