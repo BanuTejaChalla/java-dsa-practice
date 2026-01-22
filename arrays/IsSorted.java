77759df51999f1f0da5b4998f068c53b759cdb75
Banu Teja Challa <banuteja2005@gmail.com>
2026-01-22 23:33:15 +0530

    Added comments to the program that checks if a given array is sorted

--- arrays/IsSorted.java
+++ arrays/IsSorted.java
@@ -1,11 +1,12 @@
 import java.util.*;
 class IsSorted
 {
-    public static void main (String[] args) throws java.lang.Exception
+    public static void main (String[] args)
     {
 	Scanner scn = new Scanner(System.in);
 	int size = scn.nextInt();
 	int[] arr = new int[size];
+	//assumes that the array is already sorted.
 	boolean isTrue = true;
 	
 	for (int i = 0; i < size; i++)
@@ -15,8 +16,10 @@ class IsSorted
 	
 	for(int i = 0; i < size-1; i++)
 	{
+	    //checks for any singular pair that proves the assumption that the array is sorted, wrong
 	    if (arr[i] > arr[i+1])
 	    {
+		//if any pair found, then mark the boolean variable as false and print that in the final output
 	        isTrue = false;
 	    }
 	}
