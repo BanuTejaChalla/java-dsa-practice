7856f56d0f71fc4792f3958fb91a0055f9a16f7f
Banu Teja Challa <banuteja2005@gmail.com>
2026-01-23 23:29:58 +0530

    Added comments to the program that counts the number of appearences of a given key in an array

--- arrays/CountKey.java
+++ arrays/CountKey.java
@@ -7,7 +7,9 @@ class CountKey
 	Scanner scn = new Scanner(System.in);
 	int size = scn.nextInt();
 	int key = scn.nextInt();
+	// Take input for a key element that has to be counted
 	int count = 0;
+	//Create a variable count to count the nummber of appearances
 	int[] arr = new int[size];
         for (int i = 0; i < size; i++)
         {
@@ -16,10 +18,12 @@ class CountKey
 	for (int i = 0; i < arr.length; i++)
 	{
 	    if(arr[i] == key)
+	    // If the current element of the array matches the key then, increase the count by one, else do nothing
 	    {
 	        count++;
 	    }
 	}
+        //print the final count after the entire array is traversed
 	System.out.println(count);
     }
 }
