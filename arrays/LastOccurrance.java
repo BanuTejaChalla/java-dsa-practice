38c5cd64d351b8d6c73f7d94c3be84cbb8b37bdf
Banu Teja Challa <banuteja2005@gmail.com>
2026-01-25 13:37:09 +0530

    Added comments to the code that returns the index of the last occurrance of a key an the array

--- arrays/LastOccurrance.java
+++ arrays/LastOccurrance.java
@@ -1,26 +1,33 @@
-import java.util.*;
-class LastOccurrance
+import java.util.*;   
+class LastOccurrance   
 {
     public static void main (String[] args) 
     {
-	Scanner scn = new Scanner(System.in);
-	int size = scn.nextInt();
-	int key = scn.nextInt();
-        int[] arr = new int[size];
-        int res = -1;
-	for(int i = 0; i < size; i++)
-	{
-	    arr[i] = scn.nextInt();
- 	}
+        Scanner scn = new Scanner(System.in);        
+        int size = scn.nextInt();
+        int key = scn.nextInt;
+        int[] arr = new int[size];              
+        int res = -1;                           
+	// Store result index, -1 means not found
+        for(int i = 0; i < size; i++)
+        {
+            arr[i] = scn.nextInt();
+        }
        
+        // Traverse array from right to left to find last occurrence
         for(int i = size-1; i >= 0; i--)
         {
-            if(arr[i] == key)
+            if(arr[i] == key)                   
+	    // If key matches current element
             {
                 res = i;
+		// Store index of last occurrence
                 break;
+		// Stop immediately after finding it
             }
         }
-    System.out.println(res);
+        
+        System.out.println(res);
+	// Print result index
     }
 }
