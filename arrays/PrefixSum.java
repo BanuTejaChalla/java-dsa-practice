985dc406898c91cdaaff710cd50f80e4cca99d7a
Banu Teja Challa <banuteja2005@gmail.com>
2026-01-26 12:06:10 +0530

    Added comments to the PrefixSum program

--- arrays/PrefixSum.java
+++ arrays/PrefixSum.java
@@ -7,7 +7,7 @@ class PrefixSum
 	Scanner scn = new Scanner(System.in);
 	int size = scn.nextInt();
 	int[] arr = new int[size];
-	int[] prefix = new int[size];
+	int[] prefix = new int[size];			//creates a new array for the prefix sum of the same length as arr
 	int sum = 0;
 	
 	for(int i = 0; i < size; i++)
@@ -16,9 +16,9 @@ class PrefixSum
 	}
 	for(int i = 0; i < size; i++)
 	{
-	    sum = sum + arr[i];
-	    prefix[i] = sum;
-	    System.out.print(prefix[i] + " ");
+	    sum = sum + arr[i];				//sum is set to the addition of all the elements in arr until i index
+	    prefix[i] = sum;				//the ith index of prefix sum array is se to sum value
+	    System.out.print(prefix[i] + " ");		//prints the prefix array (in an unconventional way)
 	}
     }
 }
